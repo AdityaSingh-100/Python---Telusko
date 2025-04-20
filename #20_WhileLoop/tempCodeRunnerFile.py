a2d48278e56980916f966cdee7195864868be446
AdityaSingh <adityasingh902491@gmail.com>
2025-04-20 14:07:05 +0530

    nothing

---
+++ #20_WhileLoop/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+i = 5
+
+while i >= 1:
+   print("Adi",i)
+   i = i-1
---
+++ #22_BreakContinuePass/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+a = 0
+b = 1
+
+for i in range(0,50):
+    print(a)
+    c = a+b
+    a = b
+    b = c
+    if a>50:
+        break
